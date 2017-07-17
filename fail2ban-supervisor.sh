d5366e0ed24452bcc73a37505da83cc461601e0d
Nicklas Ring <niklatz@gmail.com>
2017-07-17 11:17:40 +0200

    Update fail2ban-supervisor.sh
    Jails isn't always read when process is started. Gave me a big headache.

--- fail2ban-supervisor.sh
+++ fail2ban-supervisor.sh
@@ -9,5 +9,5 @@ trap 'on_die' TERM
 # This simulates the normal startup, only in the foreground
 # Not using fail2ban-client to ensure supervisor catches any exits
 /usr/bin/python /usr/bin/fail2ban-server -f -s /var/run/fail2ban/fail2ban.sock &
-fail2ban-client reload
+sleep 1;fail2ban-client reload
 wait
